94640c7e1f69104f9402fb771ccac110ffb71b4e
amanecopse <amanecopse@users.noreply.github.com>
2022-09-23 20:21:00 +0900

    Use winston for logging
    전역 로거로 winston 모듈을 사용

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import * as dotenv from 'dotenv';
 import * as path from 'path';
+import {
+  utilities as nestWinstonModuleUtilities,
+  WinstonModule,
+} from 'nest-winston';
+import * as winston from 'winston';
 
 dotenv.config({
   path: path.resolve(`${__dirname}/config/env/.${process.env.NODE_ENV}.env`),
@@ -9,7 +14,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger({
+      transports: [
+        new winston.transports.Console({
+          level: process.env.NODE_ENV === 'production' ? 'info' : 'silly',
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            nestWinstonModuleUtilities.format.nestLike('GNUVILLAGE', {
+              prettyPrint: true,
+            }),
+          ),
+        }),
+      ],
+    }),
+  });
   await app.listen(3000);
 }
 bootstrap();
