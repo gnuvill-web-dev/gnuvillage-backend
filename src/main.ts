56d91e451893f4b4c998576fdbe4a191b0d9586a
amanecopse <amanecopse@users.noreply.github.com>
2022-09-23 20:37:25 +0900

    Use ValidationPipe
    ValidationPipe를 유효성 검사를 위해 전역적으로 사용

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ dotenv.config({
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -29,6 +30,11 @@ async function bootstrap() {
       ],
     }),
   });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
