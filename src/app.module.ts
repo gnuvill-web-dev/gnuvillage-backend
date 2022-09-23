4b159b3f19a47a304bddab9d3a5e220f1261539e
amanecopse <amanecopse@users.noreply.github.com>
2022-09-23 20:01:43 +0900

    Fix groups folder directory

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
 import { PostsModule } from './modules/posts/posts.module';
-import { GroupsModule } from './groups/groups.module';
+import { GroupsModule } from './modules/groups/groups.module';
 import { RoadmapsModule } from './modules/roadmaps/roadmaps.module';
 import { SubscriptionsModule } from './modules/subscriptions/subscriptions.module';
 import { AuthModule } from './common/auth/auth.module';
