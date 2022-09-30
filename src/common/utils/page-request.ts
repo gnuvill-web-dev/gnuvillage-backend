5a3eb629b52ae2923db93d0627d8a96bf32bf91a
amanecopse <amanecopse@users.noreply.github.com>
2022-09-30 14:15:29 +0900

    Implement pagination
    쿼리하는 객체들의 페이지네이션을 구현하기 위한 코드를 page-request.ts에 작성

---
+++ src/common/utils/page-request.ts
@@ -0,0 +1,51 @@
+import { IsOptional, IsString } from 'class-validator';
+
+export class PageRequest {
+  @IsString()
+  @IsOptional()
+  pageNo?: number | 1;
+
+  @IsString()
+  @IsOptional()
+  pageSize?: number | 10;
+
+  getOffset(): number {
+    if (this.pageNo < 1 || this.pageNo === null || this.pageNo === undefined) {
+      this.pageNo = 1;
+    }
+
+    if (
+      this.pageSize < 1 ||
+      this.pageSize === null ||
+      this.pageSize === undefined
+    ) {
+      this.pageSize = 10;
+    }
+
+    return (Number(this.pageNo) - 1) * Number(this.pageSize);
+  }
+
+  getLimit(): number {
+    if (
+      this.pageSize < 1 ||
+      this.pageSize === null ||
+      this.pageSize === undefined
+    ) {
+      this.pageSize = 10;
+    }
+    return Number(this.pageSize);
+  }
+}
+
+export class Page<T> {
+  pageSize: number;
+  totalCount: number;
+  totalPage: number;
+  items: T[];
+  constructor(totalCount: number, pageSize: number, items: T[]) {
+    this.pageSize = pageSize;
+    this.totalCount = totalCount;
+    this.totalPage = Math.ceil(totalCount / pageSize);
+    this.items = items;
+  }
+}
